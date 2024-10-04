087769d113579490ee59466f3926f1e7daa05b92
Dominik Marchewicz <marchewiczd@gmail.com>
2024-10-04 17:14:50 +0200

    moved webservice to web ui project

---
+++ Crossroads.WebUI/Services/IWebApiService.cs
@@ -0,0 +1,12 @@
+﻿namespace Crossroads.WebUI.Services;
+
+public interface IWebApiService : IDisposable
+{
+    public Task<string> GetAsync(string resource);
+
+    public Task<T> GetAsync<T>(string resource) where T : new();
+
+    public Task PostAsync<T>(string resource, T data);
+
+    public Task<bool> DeleteAsync(string resource, int id);
+}
---
+++ Crossroads.WebUI/Services/WebApiService.cs
@@ -0,0 +1,39 @@
+﻿using Crossroads.Utils.Helpers.Enums;
+using Environment = Crossroads.Utils.Helpers.Environment;
+
+namespace Crossroads.WebUI.Services;
+
+public class WebApiService(ILogger<WebApiService> logger) : IWebApiService
+{
+    private readonly HttpClient _httpClient = new();
+    private readonly Uri? _baseUri = Environment.GetUri();
+
+    public async Task<string> GetAsync(string resource)
+    {
+        var response = await _httpClient.GetAsync($"{_baseUri}{resource}");
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    public async Task<T> GetAsync<T>(string resource) where T : new()
+    {
+        var response = await _httpClient.GetAsync($"{_baseUri}{resource}");
+        return await response.Content.ReadFromJsonAsync<T>() ?? new T();
+    }
+
+    public async Task PostAsync<T>(string resource, T data)
+    {
+        await _httpClient.PostAsJsonAsync($"{_baseUri}{resource}", data);
+    }
+
+    public async Task<bool> DeleteAsync(string resource, int id)
+    {
+        var response = await _httpClient.DeleteAsync($"{_baseUri}{resource}?id={id}");
+        return await response.Content.ReadFromJsonAsync<bool>();
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        _httpClient.Dispose();
+    }
+}
