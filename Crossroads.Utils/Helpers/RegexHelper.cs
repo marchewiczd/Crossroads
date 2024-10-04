9b3957585f6fa3f74c21383b21beaf36a30fe6dd
Dominik Marchewicz <marchewiczd@gmail.com>
2024-10-04 19:22:27 +0200

    Added regex helper

---
+++ Crossroads.Utils/Helpers/RegexHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Crossroads.Utils.Helpers;
+
+public static partial class RegexHelper
+{
+    [GeneratedRegex("(\\d{4}|443|80)-")]
+    public static partial Regex Port();
+}
