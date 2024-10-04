ff39c9990a081af068f0d7707e4f2f0881704a63
Dominik Marchewicz <marchewiczd@gmail.com>
2024-10-04 19:23:29 +0200

    added missing methods to context

--- Crossroads.Database.Context/CrossroadsContext.cs
+++ Crossroads.Database.Context/CrossroadsContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Linq.Expressions;
+using System.Reflection;
 using Crossroads.Database.Entities;
 using Crossroads.Database.Entities.Abstractions;
 using Microsoft.EntityFrameworkCore;
@@ -29,8 +30,8 @@ public class CrossroadsContext : DbContext, ICrossroadsContext
     #region DbSets
     // ReSharper disable UnusedMember.Local
     
-    private DbSet<DockerNameMapping> DockerNameMappings { get; init; }
-    private DbSet<CustomContainerInfo> CustomContainerInfos { get; init; }
+    private DbSet<DockerNameMapping> DockerNameMappingSet { get; init; }
+    private DbSet<CustomContainerInfo> CustomContainerInfoSet { get; init; }
     
     // ReSharper restore UnusedMember.Local
     #endregion
@@ -66,11 +67,20 @@ public class CrossroadsContext : DbContext, ICrossroadsContext
     public async Task<T?> GetAsync<T>(object key) where T : TableBase
     {
         var dbSet = GetDbSet<T>();
-        return dbSet is null ? null : await dbSet.FindAsync(key);;
+        return dbSet is null ? null : await dbSet.FindAsync(key);
+    }
+
+    public async Task<T?> GetAsync<T>(Expression<Func<T, bool>> expression) where T : TableBase
+    {
+        var dbSet = GetDbSet<T>();
+        return dbSet is null ? null : await dbSet.FirstOrDefaultAsync(expression);
     }
 
     public T? Get<T>(object key) where T : TableBase => 
         GetDbSet<T>()?.Find(key);
+
+    public T? Get<T>(Expression<Func<T, bool>> expression) where T : TableBase => 
+        GetDbSet<T>()?.FirstOrDefault(expression);
 
     public List<T>? GetAll<T>() where T : TableBase => 
         GetDbSet<T>()?.ToList();
@@ -84,9 +94,22 @@ public class CrossroadsContext : DbContext, ICrossroadsContext
             return false;
         
         dbSet.Remove(record);
-        SaveChanges();
             
-        return true;
+        return SaveChanges() != 0;
+    }
+
+    public async Task<bool> DeleteAsync<T>(object key) where T : TableBase
+    {
+        var dbSet = GetDbSet<T>();
+        if (dbSet is null)
+            return false;
+        
+        var record = await dbSet.FindAsync(key);
+        if (record is null) 
+            return false;
+        
+        dbSet.Remove(record);
+        return await SaveChangesAsync() != 0;
     }
         
     public new bool Update<T>(T record) where T : TableBase
@@ -100,12 +123,34 @@ public class CrossroadsContext : DbContext, ICrossroadsContext
         return true;
     }
 
+    public bool Insert<T>(T entity) where T : TableBase
+    {
+        var dbSet = GetDbSet<T>();
+
+        if (dbSet is null)
+            return false;
+        
+        dbSet.Add(entity);
+        return SaveChanges() != 0;
+    }
+
+    public async Task<bool> InsertAsync<T>(T entity) where T : TableBase
+    {
+        var dbSet = GetDbSet<T>();
+
+        if (dbSet is null)
+            return false;
+        
+        await dbSet.AddAsync(entity);
+        return await SaveChangesAsync() != 0;
+    }
+
     #endregion
 
     #region Private methods
 
     private DbSet<T>? GetDbSet<T>() where T : TableBase =>
-        GetType().GetProperty(typeof(T).Name, BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(this, null) as DbSet<T>;
+        GetType().GetProperty($"{typeof(T).Name}Set", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(this, null) as DbSet<T>;
 
     #endregion
 }
--- Crossroads.Database.Context/ICrossroadsContext.cs
+++ Crossroads.Database.Context/ICrossroadsContext.cs
@@ -1,12 +1,18 @@
-﻿using Crossroads.Database.Entities.Abstractions;
+﻿using System.Linq.Expressions;
+using Crossroads.Database.Entities.Abstractions;
 
 namespace Crossroads.Database.Context;
 
 public interface ICrossroadsContext
 {
     public Task<T?> GetAsync<T>(object key) where T : TableBase;
+    public Task<T?> GetAsync<T>(Expression<Func<T, bool>> expression) where T : TableBase;
     public T? Get<T>(object key) where T : TableBase;
+    public T? Get<T>(Expression<Func<T, bool>> expression) where T : TableBase;
     public List<T>? GetAll<T>() where T : TableBase;
     public bool Delete<T>(object key) where T : TableBase;
+    public Task<bool> DeleteAsync<T>(object key) where T : TableBase;
     public bool Update<T>(T record) where T : TableBase;
+    public bool Insert<T>(T entity) where T : TableBase;
+    public Task<bool> InsertAsync<T>(T entity) where T : TableBase;
 }
